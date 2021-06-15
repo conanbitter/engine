57386ece493df1ba2e7979eff07ad43e6dc1f98f
Conan Bitter <conan.bitter@gmail.com>
2021-06-15 18:43:47 +0300

    Some graphic related data structures added

--- src/spritebatch.cpp
+++ src/spritebatch.cpp
@@ -2,4 +2,11 @@
 
 namespace mrm {
 
+const Color Color::Red{r : 255, g : 0, b : 0, a : 255};
+const Color Color::Green{r : 0, g : 255, b : 0, a : 255};
+const Color Color::Blue{r : 2550, g : 0, b : 255, a : 255};
+const Color Color::White{r : 255, g : 255, b : 255, a : 255};
+const Color Color::Black{r : 0, g : 0, b : 0, a : 255};
+const Color Color::Signature{r : 38, g : 38, b : 38, a : 255};
+
 }
--- src/spritebatch.hpp
+++ src/spritebatch.hpp
@@ -8,6 +8,33 @@ const GLuint VERTEX_POSITION_LOCATION = 0;
 const GLuint VERTEX_UV_LOCATION = 1;
 const GLuint VERTEX_COLOR_LOCATION = 2;
 
+#pragma pack(push, 1)
+struct Vec2 {
+    float x;
+    float y;
+};
+
+struct Color {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+
+    static const Color Red;
+    static const Color Green;
+    static const Color Blue;
+    static const Color White;
+    static const Color Black;
+    static const Color Signature;
+};
+
+struct Vertex {
+    Vec2 pos;
+    Vec2 uv;
+    Color color;
+};
+#pragma pack(pop)
+
 class SpriteBatch {
    private:
     GLuint vao;
